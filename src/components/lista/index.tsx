efbc4bde237bd49b821c69ee0f9e662aef10b9a1
Alisson Jaques <116194131+AlissonJaquesLocX@users.noreply.github.com>
2022-10-31 14:27:25 -0300

    Melhoria no estilo do cronômetro

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
 
 function Lista() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     }, {
-        tarefa: 'Javascript',
+        tarefa: 'JavaScript',
         tempo: '01:00:00'
-    }]
+    }, {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]);
 
     return (
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
+            <h2 onClick={() => {setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])}}> 
+                Estudos do dia
+            </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item
@@ -22,7 +27,7 @@ function Lista() {
                     />
                 ))}
             </ul>
-        </aside>
+        </aside >
     )
 }
 
