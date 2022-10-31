5180ddc45fe66611de770c006fc1c25f60d6d3e5
Alisson Jaques <116194131+AlissonJaquesLocX@users.noreply.github.com>
2022-10-31 17:19:19 -0300

    Inserindo eventos de adicionar tarefas utilizando useState

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/formulario';
 import Lista from '../components/lista';
 import style from './App.module.scss';
+import { ITarefa } from '../types/tarefas';
+import { useState } from 'react';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
