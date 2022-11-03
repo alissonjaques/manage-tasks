9909658f32d4e9abac49d997d59c2a7e1effa5f9
Alisson Jaques <116194131+AlissonJaquesLocX@users.noreply.github.com>
2022-11-03 16:02:15 -0300

    Inserindo lógica de controle da tarefa, botão començar com contador e marcação de tarefa concluída

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -3,12 +3,13 @@ import style from './Botao.module.scss';
 
 class Botao extends React.Component <{
     children: React.ReactNode,
-    type? : "button" | "submit" | "reset" | undefined
+    type? : "button" | "submit" | "reset" | undefined,
+    onClick? : () => void
     }>{
     render() {
-        const { type = "button" } = this.props;
+        const { type = "button", onClick } = this.props;
         return (
-            <button className={style.botao} type={type}>
+            <button className={style.botao} type={type} onClick={onClick}>
                 {this.props.children}
             </button>
         )
