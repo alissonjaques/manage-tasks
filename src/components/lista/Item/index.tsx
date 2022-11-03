eeefb8e7948d7a7aa9b74b4939c846f14e7fe0b9
Alisson Jaques <116194131+AlissonJaquesLocX@users.noreply.github.com>
2022-11-03 10:26:57 -0300

    Criação de funcionalidade que destaque, visualmente, quando o item é selecionado na lista de tarefas

--- src/components/lista/Item/index.tsx
+++ src/components/lista/Item/index.tsx
@@ -1,11 +1,25 @@
 import { ITarefa } from '../../../types/tarefas';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo, selecionado, completado, id}: ITarefa) {
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa}: Props) {
+    
     return (
-        <li className = { style.item }>
-            <h3> {tarefa} </h3>
-            <span> {tempo} </span>
+        <li 
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+            onClick={() => selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id
+                }                
+            )}>
+                <h3> {tarefa} </h3>
+                <span> {tempo} </span>
         </li>
-      )
+    )
 }
